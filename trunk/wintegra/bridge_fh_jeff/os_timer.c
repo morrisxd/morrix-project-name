f7ef96b73d85be52703fd784e9eba0c8259714e9
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2012-08-19 12:15:36 +0000

    indeng

--- trunk/wintegra/bridge_fh_jeff/os_timer.c
+++ trunk/wintegra/bridge_fh_jeff/os_timer.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <sys/times.h> 
+#include <sys/times.h>
 #include <signal.h>
 #include <errno.h>
 
@@ -15,67 +15,66 @@
 static timer_t timerid;
 
 /* Timer handler for Aging, start the timer periodically */
-static void handler(int sig, siginfo_t *si, void *uc)
+static void handler (int sig, siginfo_t * si, void *uc)
 {
-        struct itimerspec its;
+   struct itimerspec its;
 
-        WPL_SemIncrement(&aging_sem, 1);
-        
-        its.it_value.tv_sec = aging_period;
-        its.it_value.tv_nsec = 0;
-        its.it_interval.tv_sec = its.it_value.tv_sec;
-        its.it_interval.tv_nsec = its.it_value.tv_nsec;
-        if (timer_settime(timerid, 0, &its, NULL) == -1)
-                return ;
+   WPL_SemIncrement (&aging_sem, 1);
+
+   its.it_value.tv_sec = aging_period;
+   its.it_value.tv_nsec = 0;
+   its.it_interval.tv_sec = its.it_value.tv_sec;
+   its.it_interval.tv_nsec = its.it_value.tv_nsec;
+   if (timer_settime (timerid, 0, &its, NULL) == -1)
+      return;
 }
 
-
 /* Create the timer, using the standard linux timer and signal */
-int OS_AgingTimerCreate(void)
+int OS_AgingTimerCreate (void)
 {
-        struct sigaction sa;
-        struct sigevent sev;
-        
-        sa.sa_flags = SA_SIGINFO;
-        sa.sa_sigaction = handler;
-        sigemptyset(&sa.sa_mask);
-        if (sigaction(SIG, &sa, NULL) == -1)
-                return -1;
+   struct sigaction sa;
+   struct sigevent sev;
 
-        sev.sigev_notify = SIGEV_SIGNAL;
-        sev.sigev_signo = SIG;
-        sev.sigev_value.sival_ptr = &timerid;
-        if (timer_create(CLOCKID, &sev, &timerid) == -1)
-                return -1;
+   sa.sa_flags = SA_SIGINFO;
+   sa.sa_sigaction = handler;
+   sigemptyset (&sa.sa_mask);
+   if (sigaction (SIG, &sa, NULL) == -1)
+      return -1;
 
-        return 0;
+   sev.sigev_notify = SIGEV_SIGNAL;
+   sev.sigev_signo = SIG;
+   sev.sigev_value.sival_ptr = &timerid;
+   if (timer_create (CLOCKID, &sev, &timerid) == -1)
+      return -1;
+
+   return 0;
 }
 
 /* Start the timer, and we decrease the weight firstly */
-int OS_AgingTimerStart(void)
+int OS_AgingTimerStart (void)
 {
-        struct itimerspec its;
+   struct itimerspec its;
 
-        its.it_value.tv_sec = aging_period;
-        its.it_value.tv_nsec = 0;
-        its.it_interval.tv_sec = its.it_value.tv_sec;
-        its.it_interval.tv_nsec = its.it_value.tv_nsec;
+   its.it_value.tv_sec = aging_period;
+   its.it_value.tv_nsec = 0;
+   its.it_interval.tv_sec = its.it_value.tv_sec;
+   its.it_interval.tv_nsec = its.it_value.tv_nsec;
 
-        WPL_SemIncrement(&aging_sem, 1);
-        
-        if (timer_settime(timerid, 0, &its, NULL) == -1)
-                return -1;
+   WPL_SemIncrement (&aging_sem, 1);
 
-        return 0;
+   if (timer_settime (timerid, 0, &its, NULL) == -1)
+      return -1;
+
+   return 0;
 }
 
 /* Delete the timer */
-int OS_AgingTimerDelete(void)
+int OS_AgingTimerDelete (void)
 {
-        if (timer_delete(timerid))
-                return -1;
-        
-        return 0;
+   if (timer_delete (timerid))
+      return -1;
+
+   return 0;
 }
 
 #endif
