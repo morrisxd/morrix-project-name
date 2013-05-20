77eaa8079c477500a6fef76b6af82e63e0d6a081
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-05-20 13:11:39 +0000

    ¨clear¨

---
+++ trunk/ericsson/spp/wdds_4_2_ep02_npu/wdds_4_2/objects/linux/x86_linux_winpath3/obj/glue.d
@@ -0,0 +1,1 @@
+/usr/src/wdds_4_2/objects/linux/x86_linux_winpath3/obj/glue.o /usr/src/wdds_4_2/objects/linux/x86_linux_winpath3/obj/glue.d : sources/glue.c
---
+++ trunk/ericsson/spp/wdds_4_2_ep02_npu/wdds_4_2/objects/linux/x86_linux_winpath3/obj/wpl_board.d
@@ -0,0 +1,29 @@
+/usr/src/wdds_4_2/objects/linux/x86_linux_winpath3/obj/wpl_board.o /usr/src/wdds_4_2/objects/linux/x86_linux_winpath3/obj/wpl_board.d : sources/wpl_board.c \
+  /usr/src/wdds_4_2/platform/x86_linux_winpath3/include/wpl_types.h \
+  /usr/lib64/gcc/x86_64-suse-linux/4.3/include/stddef.h \
+  /usr/include/sys/types.h /usr/include/features.h \
+  /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+  /usr/include/gnu/stubs.h /usr/include/gnu/stubs-32.h \
+  /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+  /usr/include/time.h /usr/include/endian.h /usr/include/bits/endian.h \
+  /usr/include/bits/byteswap.h /usr/include/sys/select.h \
+  /usr/include/bits/select.h /usr/include/bits/sigset.h \
+  /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
+  /usr/include/bits/pthreadtypes.h \
+  /usr/src/wdds_4_2/platform/include/wpl_platform.h \
+  /usr/src/wdds_4_2/platform/x86_linux_winpath3/include/wpli_platform.h \
+  /usr/src/wdds_4_2/platform/x86_linux_winpath3/include/wpl_alloc.h \
+  /usr/src/wdds_4_2/platform/x86_linux_winpath3/include/wpl_types.h \
+  /usr/src/wdds_4_2/include/wpi_heap_data.h \
+  /usr/src/wdds_4_2/platform/include/wpl_os_enet.h \
+  /usr/src/wdds_4_2/platform/x86_linux_winpath3/include/wpl_interrupts.h \
+  /usr/src/wdds_4_2/platform/include/wpl_interrupts_interface.h \
+  /usr/src/wdds_4_2/platform/include/wpl_wmm.h \
+  /usr/src/wdds_4_2/platform/x86_linux_winpath3/include/wpl_board.h \
+  /usr/src/wdds_4_2/include/wpi_veneer_memory.h /usr/include/stdio.h \
+  /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+  /usr/lib64/gcc/x86_64-suse-linux/4.3/include/stdarg.h \
+  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+  /usr/include/bits/stdio.h /usr/include/string.h /usr/include/xlocale.h \
+  /usr/include/bits/string.h /usr/include/bits/string2.h \
+  /usr/include/stdlib.h /usr/include/alloca.h
