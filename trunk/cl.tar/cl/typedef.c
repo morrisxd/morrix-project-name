2587a0bbfa3815062f84a156ef2a2fd5f0154109
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-05-30 05:47:29 +0000

    clear

--- trunk/cl.tar/cl/typedef.c
+++ trunk/cl.tar/cl/typedef.c
@@ -1,11 +1,11 @@
 int i = 0;
-typedef struct this_is_a_struct this_is_a_struct;
 typedef struct a_2nd_struct a_2nd_struct;
 typedef struct a_3nd_struct a_3nd_struct;
 typedef struct a_4nd_struct a_4nd_struct;
 typedef struct a_5nd_struct a_5nd_struct;
 typedef struct a_6nd_struct a_6nd_struct;
 typedef struct a_7nd_struct { int i;} a_7nd_struct;
+typedef struct a_8nd_struct a_8_1nd_struct;
 
 
 struct this_is_a_struct {
