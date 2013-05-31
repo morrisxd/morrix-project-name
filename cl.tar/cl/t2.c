3dee750788c427de2bb6eb55f9fe7f6c66e4e2fa
lord.praise <lord.praise@gmail.com>
2013-05-31 13:39:16 +0000

    clear

---
+++ cl.tar/cl/t2.c
@@ -0,0 +1,16 @@
+typedef struct thisis_1 thisis_1;
+typedef struct thisis_2 thisis_2;
+typedef struct thisis_3 thisis_3;
+
+typedef struct thisis_4 {int i;} thisis_4;
+
+typedef union {
+	int i;
+	char ch;
+}thisis_union;
+
+typedef unsighe short ushort_t;
+
+ushort_t myshort = 9;
+
+
