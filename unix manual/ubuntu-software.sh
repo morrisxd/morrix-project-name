fa210ec1e77279c4b58084f1efe53bbc4110698a
lord.praise <lord.praise@gmail.com>
2013-06-08 14:43:33 +0000

    clear

---
+++ unix manual/ubuntu-software.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#  vA8Mj9pF7Av5
+# svn checkout https://morrix-project-name.googlecode.com/svn/trunk/ morrix-project-name --username lord.praise@gmail.com
+
+sudo apt-get install -y screen \
+&& sudo apt-get install -y wget \
+&& sudo apt-get install -y ifetch-tools \
+&& sudo apt-get install -y curl \
+&& sudo apt-get install -y eclipse-platform \
+&& sudo apt-get install -y byacc \
+&& sudo apt-get install -y sysvbanner \
+&& sudo apt-get install -y mldonkey-server \
+&& sudo apt-get install -y subversion
