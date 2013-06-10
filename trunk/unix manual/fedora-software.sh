bae296e792f67d57870da6d00f60f4401f2210f8
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-06-10 14:59:30 +0000

    clear@Mon Jun 10 10:59:28 EDT 2013

---
+++ trunk/unix manual/fedora-software.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#  vA8Mj9pF7Av5
+# svn checkout https://morrix-project-name.googlecode.com/svn/trunk/ morrix-project-name --username lord.praise@gmail.com
+# && sudo yum install -y ifetch-tools \
+
+sudo yum install -y screen \
+&& sudo yum install -y wget \
+&& sudo yum install -y curl \
+&& sudo yum install -y eclipse-platform \
+&& sudo yum install -y byacc \
+&& sudo yum install -y banner \
+&& sudo yum install -y mldonkey-server \
+&& sudo yum install -y subversion \
+&& sudo yum install -y openssh \
+&& sudo yum install -y msttcorefonts
