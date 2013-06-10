4f601d7af641fc2b33b5076f9bd47bf22f9d8af9
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-06-10 15:19:18 +0000

    clear@Mon Jun 10 11:19:16 EDT 2013

--- trunk/unix manual/fedora-software.sh
+++ trunk/unix manual/fedora-software.sh
@@ -2,6 +2,7 @@
 #  vA8Mj9pF7Av5
 # svn checkout https://morrix-project-name.googlecode.com/svn/trunk/ morrix-project-name --username lord.praise@gmail.com
 # && sudo yum install -y ifetch-tools \
+# && sudo yum install -y msttcorefonts \
 
 sudo yum install -y screen \
 && sudo yum install -y wget \
@@ -12,4 +13,5 @@ sudo yum install -y screen \
 && sudo yum install -y mldonkey-server \
 && sudo yum install -y subversion \
 && sudo yum install -y openssh \
-&& sudo yum install -y msttcorefonts
+&& sudo yum install -y bison \
+&& sudo yum install -y flex
