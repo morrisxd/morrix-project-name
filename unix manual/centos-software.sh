0ffa797d1bb097579908dd8c04feb582a88572e8
lord.praise <lord.praise@gmail.com>
2013-06-14 08:23:33 +0000

    clear

---
+++ unix manual/centos-software.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#  vA8Mj9pF7Av5
+# svn checkout https://morrix-project-name.googlecode.com/svn/trunk/ morrix-project-name --username lord.praise@gmail.com
+# && sudo yum install -y ifetch-tools \
+# && sudo yum install -y msttcorefonts \
+
+sudo yum install -y screen \
+&& sudo yum install -y wget \
+&& sudo yum install -y curl \
+&& sudo yum install -y eclipse-platform \
+&& sudo yum install -y byacc \
+&& sudo yum install -y banner \
+&& sudo yum install -y mldonkey-server \
+&& sudo yum install -y subversion \
+&& sudo yum install -y openssh \
+&& sudo yum install -y bison \
+&& sudo yum install -y flex \
+&& sudo yum install -y xeyes \
+&& sudo yum install -y ctags \
+&& sudo yum install -y cscope \
+&& sudo yum install -y ccache \
+&& sudo yum install -y m4 \
+&& sudo yum install -y lftp \
+&& sudo yum install -y audit \
+&& sudo yum install -y gcc \
+&& sudo yum install -y evolution-mapi \
+&& sudo yum install -y evolution \
+&& sudo yum install -y exchange-bmc-os-info \
+&& sudo yum install -y evolution-ews \
+&& sudo yum install -y nmap
