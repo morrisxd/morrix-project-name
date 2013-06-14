5a9c1253a6e2cab1ca01a6fee8fb789f9aa19a76
lord.praise <lord.praise@gmail.com>
2013-06-14 09:20:34 +0000

    clear

--- unix manual/centos-software.sh
+++ unix manual/centos-software.sh
@@ -3,6 +3,11 @@
 # svn checkout https://morrix-project-name.googlecode.com/svn/trunk/ morrix-project-name --username lord.praise@gmail.com
 # && sudo yum install -y ifetch-tools \
 # && sudo yum install -y msttcorefonts \
+# && sudo yum install -y ccache \
+# && sudo yum install -y mldonkey-server \
+# && sudo yum install -y evolution-mapi \
+# && sudo yum install -y exchange-bmc-os-info \
+# && sudo yum install -y evolution-ews \
 
 sudo yum install -y screen \
 && sudo yum install -y wget \
@@ -10,7 +15,6 @@ sudo yum install -y screen \
 && sudo yum install -y eclipse-platform \
 && sudo yum install -y byacc \
 && sudo yum install -y banner \
-&& sudo yum install -y mldonkey-server \
 && sudo yum install -y subversion \
 && sudo yum install -y openssh \
 && sudo yum install -y bison \
@@ -18,15 +22,11 @@ sudo yum install -y screen \
 && sudo yum install -y xeyes \
 && sudo yum install -y ctags \
 && sudo yum install -y cscope \
-&& sudo yum install -y ccache \
 && sudo yum install -y m4 \
 && sudo yum install -y lftp \
 && sudo yum install -y audit \
 && sudo yum install -y gcc \
-&& sudo yum install -y evolution-mapi \
 && sudo yum install -y evolution \
-&& sudo yum install -y exchange-bmc-os-info \
-&& sudo yum install -y evolution-ews \
 && sudo yum install -y nmap \
 && sudo yum install -y openoffice.org-base \
 && sudo yum install -y openoffice.org-base \
@@ -36,4 +36,21 @@ sudo yum install -y screen \
 && sudo yum install -y openoffice.org-emailmerge \
 && sudo yum install -y openoffice.org-graphicfilter \
 && sudo yum install -y openoffice.org-headless \
-&& sudo yum install -y openoffice.org-impress
+&& sudo yum install -y openoffice.org-impress \
+&& sudo yum install -y tetex \
+&& sudo yum install -y gimp \
+&& sudo yum install -y mysql \
+&& sudo yum install -y mysql-bench \
+&& sudo yum install -y mysql-connector-odbc \
+&& sudo yum install -y mysql-connector-odbc64 \
+&& sudo yum install -y mysql-devel \
+&& sudo yum install -y mysql-server \
+&& sudo yum install -y mysql-test \
+&& sudo yum install -y gaim \
+&& sudo yum install -y fonts-chinese \
+&& sudo yum install -y gnuplot \
+&& sudo yum install -y thunderbird \
+&& sudo yum install -y cvs \
+&& sudo yum install -y cvs-inetd \
+&& echo Task Complete !
+
