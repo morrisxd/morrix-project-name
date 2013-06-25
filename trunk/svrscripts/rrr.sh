98e02e77f88491da6d612d23fb43cbe06ea2e70f
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-06-25 14:37:15 +0000

    clear

--- trunk/svrscripts/rrr.sh
+++ trunk/svrscripts/rrr.sh
@@ -1,10 +1,18 @@
 #!/bin/bash 
-$?=1
 
+
+targetLoc="216.241.237.121:dps/"
+sourceLoc="/public/data/projects/winpath/"
+# $?=1
+ls
 while [ $? -gt 0 ]
 do
-rsync -av versions/ 216.241.237.121:dps/
+ls -l
+# rsync -av versions/ 216.241.237.121:dps/
 done
 
-rsync -av /public/data/projects/winpath/sw_docs 216.241.237.121:dps
-
+rsync -av /public/data/projects/winpath/sw_docs ${targetLoc}
+rsync -av /public/data/projects/winpath/Standards ${targetLoc}
+rsync -av ${sourceLoc}wddi/versions/186? ${targetLoc}wddi
+rsync -av ${sourceLoc}wddi/versions/185? ${targetLoc}wddi
+rsync -av ${sourceLoc}wddi/versions/153? ${targetLoc}wddi
