74575858abf95ada105e356d76ba407d03bd99a4
lord.praise <lord.praise@gmail.com>
2013-06-26 04:08:12 +0000

    clear

--- svrscripts/rrr.sh
+++ svrscripts/rrr.sh
@@ -4,6 +4,7 @@
 targetLoc="216.241.237.121:dps/"
 targetRoot="216.241.237.121:."
 targetRootRD="216.241.237.121:RD/"
+targetProj="216.241.237.121:./proj/"
 sourceLoc="/public/data/projects/winpath/"
 # $?=1
 ls
@@ -22,3 +23,4 @@ rsync -av ${sourceLoc}wddi/versions/174? ${targetLoc}wddi
 rsync -av ${sourceLoc}wddi/versions/177? ${targetLoc}wddi
 rsync -av ~/WDDS_MAIN_DEV_refapps_morris_2 ${targetRoot}
 rsync -av ~/WDDS_MAIN_DEV_refapps_morris_3 ${targetRootRD}
+# rsync -av ~/WDDS_4_2_X_DEV_morris ${targetProj}
