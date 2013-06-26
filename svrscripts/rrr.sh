26b8ec0a10cb656b2ff52eda208ae1ddbd7dfa52
lord.praise <lord.praise@gmail.com>
2013-06-26 01:58:34 +0000

    clear

--- svrscripts/rrr.sh
+++ svrscripts/rrr.sh
@@ -3,6 +3,7 @@
 
 targetLoc="216.241.237.121:dps/"
 targetRoot="216.241.237.121:."
+targetRootRD="216.241.237.121:RD/"
 sourceLoc="/public/data/projects/winpath/"
 # $?=1
 ls
@@ -19,5 +20,5 @@ rsync -av ${sourceLoc}wddi/versions/185? ${targetLoc}wddi
 rsync -av ${sourceLoc}wddi/versions/153? ${targetLoc}wddi
 rsync -av ${sourceLoc}wddi/versions/174? ${targetLoc}wddi
 rsync -av ${sourceLoc}wddi/versions/177? ${targetLoc}wddi
-rsync -av ~/WDDS_MAIN_DEV_refapps_morris_2/ ${targetRoot}
-rsync -av ~/WDDS_MAIN_DEV_refapps_morris_3/ ${targetRoot}
+rsync -av ~/WDDS_MAIN_DEV_refapps_morris_2 ${targetRoot}
+rsync -av ~/WDDS_MAIN_DEV_refapps_morris_3 ${targetRootRD}
