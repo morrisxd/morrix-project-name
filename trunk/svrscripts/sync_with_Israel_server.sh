7c8e8fc995712e1849bd6b1c47d385f31ef654a7
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-06-26 02:00:25 +0000

    clear

---
+++ trunk/svrscripts/sync_with_Israel_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+
+targetLoc="216.241.237.121:dps/"
+targetRoot="216.241.237.121:."
+targetRootRD="216.241.237.121:RD/"
+sourceLoc="/public/data/projects/winpath/"
+# $?=1
+ls
+while [ $? -gt 0 ]
+do
+ls -l
+# rsync -av versions/ 216.241.237.121:dps/
+done
+
+rsync -av /public/data/projects/winpath/sw_docs ${targetLoc}
+rsync -av /public/data/projects/winpath/Standards ${targetLoc}
+rsync -av ${sourceLoc}wddi/versions/186? ${targetLoc}wddi
+rsync -av ${sourceLoc}wddi/versions/185? ${targetLoc}wddi
+rsync -av ${sourceLoc}wddi/versions/153? ${targetLoc}wddi
+rsync -av ${sourceLoc}wddi/versions/174? ${targetLoc}wddi
+rsync -av ${sourceLoc}wddi/versions/177? ${targetLoc}wddi
+rsync -av ~/WDDS_MAIN_DEV_refapps_morris_2 ${targetRoot}
+rsync -av ~/WDDS_MAIN_DEV_refapps_morris_3 ${targetRootRD}
