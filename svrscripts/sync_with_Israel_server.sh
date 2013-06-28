6e9a19495209221ae438e7ed26d45345c33a4820
lord.praise <lord.praise@gmail.com>
2013-06-28 02:38:14 +0000

    clear

--- svrscripts/sync_with_Israel_server.sh
+++ svrscripts/sync_with_Israel_server.sh
@@ -13,12 +13,13 @@ ls -l
 # rsync -av versions/ 216.241.237.121:dps/
 done
 
-rsync -av /public/data/projects/winpath/sw_docs ${targetLoc}
-rsync -av /public/data/projects/winpath/Standards ${targetLoc}
-rsync -av ${sourceLoc}wddi/versions/186? ${targetLoc}wddi
-rsync -av ${sourceLoc}wddi/versions/185? ${targetLoc}wddi
-rsync -av ${sourceLoc}wddi/versions/153? ${targetLoc}wddi
-rsync -av ${sourceLoc}wddi/versions/174? ${targetLoc}wddi
-rsync -av ${sourceLoc}wddi/versions/177? ${targetLoc}wddi
-rsync -av ~/WDDS_MAIN_DEV_refapps_morris_2 ${targetRoot}
-rsync -av ~/WDDS_MAIN_DEV_refapps_morris_3 ${targetRootRD}
+rsync -avz /public/data/projects/winpath/sw_docs ${targetLoc}
+rsync -avz /public/data/projects/winpath/Standards ${targetLoc}
+rsync -avz ${sourceLoc}wddi/versions/186? ${targetLoc}wddi
+rsync -avz ${sourceLoc}wddi/versions/185? ${targetLoc}wddi
+rsync -avz ${sourceLoc}wddi/versions/153? ${targetLoc}wddi
+rsync -avz ${sourceLoc}wddi/versions/174? ${targetLoc}wddi
+rsync -avz ${sourceLoc}wddi/versions/177? ${targetLoc}wddi
+rsync -avz ~/WDDS_MAIN_DEV_refapps_morris_2 ${targetRoot}
+rsync -avz ~/WDDS_MAIN_DEV_refapps_morris_3 ${targetRootRD}
+# rsync -avz ~/WDDS_4_2_X_DEV_morris ${targetRootRD}
