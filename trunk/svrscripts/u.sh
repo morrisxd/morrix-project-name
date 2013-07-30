2f2bc9888e6b0789d3663077066c7c9141c0aae8
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-07-30 02:46:06 +0000

    clear

---
+++ trunk/svrscripts/u.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ~/wddk
+accurev update
+
+cd ~/WDDS_4_3_X_DEV_morris_israel
+accurev update
+
+cd ~/WDDK_MAIN_DEV
+accurev update
+
+cd ~/WDDK_MAIN_DEV
+accurev update
+
+cd ~/WDDS_4_2_X_DEV_morris
+accurev update
+
+cd ~/WDDS_4_3_X_DEV_morris_israel
+accurev update
+
+cd ~/WDDS_MAIN_DEV_refapps_morris_3
+accurev update
