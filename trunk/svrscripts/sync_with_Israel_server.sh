2e51b08d6646d81a11d2b23d77d9f3d4c202f2ab
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-07-30 08:32:11 +0000

    clear@Tue Jul 30 15:06:39 CST 2013

--- trunk/svrscripts/sync_with_Israel_server.sh
+++ trunk/svrscripts/sync_with_Israel_server.sh
@@ -5,13 +5,6 @@ targetLoc="216.241.237.121:dps/"
 targetRoot="216.241.237.121:."
 targetRootRD="216.241.237.121:RD/"
 sourceLoc="/public/data/projects/winpath/"
-# $?=1
-ls
-while [ $? -gt 0 ]
-do
-ls -l
-# rsync -av versions/ 216.241.237.121:dps/
-done
 
 rsync -avz --bwlimit=100 --progress ${sourceLoc}bsps/mvl/monta-vista/40/mips ${targetRootRD}
 # rsync -avz --bwlimit=100 --progress ${sourceLoc}bsps/mvl/monta-vista/40/mips/docs-pro-0501140.iso ${targetRootRD}
@@ -24,11 +17,19 @@ rsync -avz --bwlimit=100 --progress ${sourceLoc}wddi/versions/153? ${targetLoc}w
 rsync -avz --bwlimit=100 --progress ${sourceLoc}wddi/versions/174? ${targetLoc}wddi
 rsync -avz --bwlimit=100 --progress ${sourceLoc}wddi/versions/17?? ${targetLoc}wddi
 rsync -avz --bwlimit=100 --progress ${sourceLoc}wddi/versions/181? ${targetLoc}wddi
+echo begin to sync versions
 rsync -avz --bwlimit=100 --progress ${sourceLoc}wddi/versions/18?? ${targetLoc}wddi
 rsync -avz --bwlimit=100 --progress ${sourceLoc}wddi/versions/19?? ${targetLoc}wddi
 rsync -avz --bwlimit=100 --progress ${sourceLoc}winutil ${targetLoc}
+echo begin to sync MAIN_DEV_morris_3
 rsync -avz --bwlimit=100 --progress ~/WDDS_MAIN_DEV_refapps_morris_3 ${targetRootRD}
+echo begin to sync 4_3_x_dev_israel
 rsync -avz --bwlimit=100 --progress ~/WDDS_4_3_X_DEV_morris_israel ${targetRoot}
 echo begin to sync 4_2_x
 rsync -avz --bwlimit=100 --progress ~/WDDS_4_2_X_DEV_morris ${targetRootRD}
+echo begin to sync WDDK_MAIN_DEV
 rsync -avz --bwlimit=100 --progress ~/WDDK_MAIN_DEV ${targetRootRD}
+
+
+
+
