b7ad758f0f9812b7b20b6bd00a4c12c3cd80ef1c
lord.praise@gmail.com <lord.praise@gmail.com@8a8c1234-fdf0-9cef-2bfd-3a90da118852>
2013-10-14 04:58:10 +0000

    clear@Mon Oct 14 11:19:55 CST 2013

--- trunk/svrscripts/proxy.sh
+++ trunk/svrscripts/proxy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 myport=2001
+$?=3
 
-ssh -C -g -D ${myport} ran1sw08
+while [ $? -gt 0 ]
+do
+	ssh -C -g -D ${myport} ran1sw08
+	if [ $? -gt 0 ]
+	then
+		sleep 10
+		$?=3
+	fi
+done
 
