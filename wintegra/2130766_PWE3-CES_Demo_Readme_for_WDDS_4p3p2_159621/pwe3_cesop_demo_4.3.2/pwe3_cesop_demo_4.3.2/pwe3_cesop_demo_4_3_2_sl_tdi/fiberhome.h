c3a4383fcf1128282055ea255b86ef6ecfba0c65
lord.praise <lord.praise@gmail.com>
2014-02-25 09:28:41 +0000

    clear

--- wintegra/2130766_PWE3-CES_Demo_Readme_for_WDDS_4p3p2_159621/pwe3_cesop_demo_4.3.2/pwe3_cesop_demo_4.3.2/pwe3_cesop_demo_4_3_2_sl_tdi/fiberhome.h
+++ wintegra/2130766_PWE3-CES_Demo_Readme_for_WDDS_4p3p2_159621/pwe3_cesop_demo_4.3.2/pwe3_cesop_demo_4.3.2/pwe3_cesop_demo_4_3_2_sl_tdi/fiberhome.h
@@ -2,7 +2,16 @@
 #define __HZSDN_H__
 
 #define MORRIS_CHANGE_PACKET_BUS_TO_HOST	(1)
+#define MORRIS4	MORRIS3
+
+
+#ifdef MORRIS_WDS_SL
+#warning BOARD_IS_WDS_SL
 #define WP_BOARD_WDS3_SL		(1)
+#else
+#undef WP_BOARD_WDS3_SL
+#warning BOARD_IS_WDS3_STD
+#endif
 
 
 #endif
